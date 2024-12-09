dfedce2fb88367b67056cf82740adc11c83b2ff9
Kyle Ross <kyle.ross@esusu.org>
2024-12-09 14:27:45 -0700

    updated name in plugin

--- mega-menu-block.php
+++ mega-menu-block.php
@@ -1,11 +1,11 @@
 <?php
 /**
- * Plugin Name:       Mega Menu Block
+ * Plugin Name:       WP Mega Menu Block
  * Description:       Example block scaffolded with Create Block tool.
  * Requires at least: 6.6
  * Requires PHP:      7.2
  * Version:           0.1.0
- * Author:            The WordPress Contributors
+ * Author:            Kyle Ross
  * License:           GPL-2.0-or-later
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * Text Domain:       mega-menu-block
