78856134e894c2555465d68d5f4fd30c4c71028d
Kyle Ross <kyle@badger3000.com>
2024-12-10 09:13:47 -0700

    admin styles saved

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => '39f9b1d6607c55b2e4ea');
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n'), 'version' => 'fb4310a4bfb72a303d62');
